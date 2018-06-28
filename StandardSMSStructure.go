5adcdd273425ff8e627bca7524982f4a490f2534
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-06-29 09:23:51 +1000

    add method in struct

--- StandardSMSStructure.go
+++ StandardSMSStructure.go
@@ -2,6 +2,7 @@ package msgstruct
 
 //StandardSMSStructure json supported structure to make a sms request
 type StandardSMSStructure struct {
+	Method         string            `json:"method"` //should be sms
 	Sender         *string           `json:"sender"`
 	Receiver       *string           `json:"receiver"`
 	TextBody       *string           `json:"text"`
